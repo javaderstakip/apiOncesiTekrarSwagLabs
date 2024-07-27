4df9eb6bd397cf28f1d4f3e8d38b378e567b6463
javaderstakip <mehmetjavaderstakip@gmail.com>
2024-07-27 20:47:30 +0300

    202407272047

--- src/test/java/org/cb/ta/test/elementsPage/WebTablesTest.java
+++ src/test/java/org/cb/ta/test/elementsPage/WebTablesTest.java
@@ -2,6 +2,9 @@ package org.cb.ta.test.elementsPage;
 
 import org.cb.ta.pages.ElementsPage;
 import org.cb.ta.test.common.BaseTest;
+import org.junit.experimental.theories.Theories;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -41,24 +44,36 @@ public class WebTablesTest extends BaseTest {
         Assert.assertTrue(driver.getCurrentUrl().equals("https://demoqa.com/webtables"));
     }
     @Test
-    public void webTablesTest(){
+    public void webTablesTest() throws InterruptedException {
         Assert.assertFalse(elementsPage.getWebTablesAdd().isSelected());
         Assert.assertTrue(elementsPage.getWebTablesAdd().isEnabled());
 
-        elementsPage.getWebTablesAdd().click();
-        //Assert.assertTrue(elementsPage.getWebTablesRegistrationForm().getText().equals("Registration Form"));
+        elementsPage.getWebTablesAdd().click();//Thread.sleep(3000);
+        Assert.assertTrue(elementsPage.getWebTablesRegistrationForm().getText().equals("Registration Form"));
+        actions.sendKeys(Keys.TAB).click().perform();//Thread.sleep(3000);
 
-        elementsPage.getWebTablesAdd().click();
-        elementsPage.getWebTablesRegistrationFormX().click();
+        elementsPage.getWebTablesAdd().click();//Thread.sleep(3000);
+        actions.sendKeys(Keys.TAB).perform();//Thread.sleep(3000);
+        actions.sendKeys(Keys.TAB).sendKeys("Tahir").perform();//Thread.sleep(3000);
+        actions.sendKeys(Keys.TAB).sendKeys("Kelb").perform();
+        actions.sendKeys(Keys.TAB).sendKeys("tk@gmail.com").perform();
+        actions.sendKeys(Keys.TAB).sendKeys("33").perform();
+        actions.sendKeys(Keys.TAB).sendKeys("2500").perform();Thread.sleep(3000);
+        actions.sendKeys(Keys.TAB).sendKeys("Legal").perform();Thread.sleep(3000);
+        actions.sendKeys(Keys.TAB).perform();Thread.sleep(3000);
+        elementsPage.getWebTablesRegistrationFormSubmit().click();Thread.sleep(3000);
+
 //        elementsPage.getWebTablesRegistrationFormFirstName().click();
 //        elementsPage.getWebTablesRegistrationFormFirstNameText().click();
-        actions.moveToElement(elementsPage.getWebTablesRegistrationFormFirstName()).click().build().perform();
-        elementsPage.getWebTablesRegistrationFormFirstName().sendKeys("Tahir");
-        elementsPage.getWebTablesRegistrationFormLastName().sendKeys("Kelb");
-        elementsPage.getWebTablesRegistrationFormEmail().sendKeys("tk@gmail.com");
-        elementsPage.getWebTablesRegistrationFormAge().sendKeys("33");
-        elementsPage.getWebTablesRegistrationFormSalary().sendKeys("2500");
-        elementsPage.getWebTablesRegistrationFormDepartment().sendKeys("Legal");
-        elementsPage.getWebTablesRegistrationFormSubmit().click();
+//        Thread.sleep(3000);
+//        actions.moveToElement(elementsPage.getWebTablesRegistrationFormFirstName()).click().build().perform();
+//        Thread.sleep(3000);
+        //elementsPage.getWebTablesRegistrationFormFirstName().sendKeys("Tahir");Thread.sleep(3000);
+//        elementsPage.getWebTablesRegistrationFormLastName().sendKeys("Kelb");
+//        elementsPage.getWebTablesRegistrationFormEmail().sendKeys("tk@gmail.com");
+//        elementsPage.getWebTablesRegistrationFormAge().sendKeys("33");
+//        elementsPage.getWebTablesRegistrationFormSalary().sendKeys("2500");
+//        elementsPage.getWebTablesRegistrationFormDepartment().sendKeys("Legal");
+//        elementsPage.getWebTablesRegistrationFormSubmit().click();
     }
 }
