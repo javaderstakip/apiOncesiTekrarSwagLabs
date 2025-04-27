6b847dad611a05742e6a8f07f1595e3570099ddd
javaderstakip <mehmetjavaderstakip@gmail.com>
2025-04-28 00:06:02 +0300

    20250428

--- src/test/java/org/cb/ta/test/formsPageTest/PracticeFormTest.java
+++ src/test/java/org/cb/ta/test/formsPageTest/PracticeFormTest.java
@@ -5,10 +5,7 @@ import org.cb.ta.test.common.BaseTest;
 import org.cb.ta.pages.ElementsPage;
 import org.cb.ta.pages.elementsPages.DynamicPropertiesPage;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
@@ -78,109 +75,6 @@ public class PracticeFormTest extends BaseTest {
                 .getStudentRegistrationFormText().getText());
     }
 
-//    @Test(priority = 2)
-//    public void practiceFormBeginningFulling() throws InterruptedException {
-//        practiceFormPage.getFirstName().click();//Thread.sleep(3000);
-//        practiceFormPage.getFirstName().sendKeys("Hakan");
-//
-//        practiceFormPage.getLastName().click();//Thread.sleep(3000);
-//        practiceFormPage.getLastName().sendKeys("Basar");
-//
-//        practiceFormPage.getEMail().click();
-//        practiceFormPage.getEMail().sendKeys("hakanbasar@gmail.com");
-//
-//        practiceFormPage.getGenderLabelMale().click();//Thread.sleep(3000);
-//        practiceFormPage.getGenderLabelFemale().click();//Thread.sleep(3000);
-//        practiceFormPage.getGenderLabelOther().click();//Thread.sleep(3000);
-//
-//        practiceFormPage.getMobileNumber().click();
-//        practiceFormPage.getMobileNumber().sendKeys("3125353535");
-//
-//        practiceFormPage.getDateOfBirth().click();//Thread.sleep(3000);
-//        practiceFormPage.getBinDokuzYuzSeksen().click();//Thread.sleep(3000);
-//        practiceFormPage.getSeptember().click();//Thread.sleep(3000);
-//        practiceFormPage.getYirmiUcuncuGun().click();//Thread.sleep(3000);
-//
-//        jsx.executeScript("window.scrollBy(0,250)");
-////        practiceFormPage.getSubjects().click();Thread.sleep(3000);
-////        practiceFormPage.getSubjects().sendKeys(Keys.ENTER);
-////        practiceFormPage.getSubjects().sendKeys("Hasar");
-////        practiceFormPage.getSubjects().sendKeys(Keys.ENTER);
-//
-//        try {
-//            practiceFormPage.getSubjects().click();Thread.sleep(3000);
-//            //practiceFormPage.getSubjects().sendKeys(Keys.ENTER);
-//            practiceFormPage.getSubjects().sendKeys("Basar");
-//            practiceFormPage.getSubjects().sendKeys(Keys.ENTER);
-//            // Subject alanını bulmaya çalış
-////            subjectInput = driver.findElement(By.id("subjectsInput"));
-////            subjectInput.sendKeys(subjectText);
-////            Assert.assertEquals(subjectInput.getAttribute("value"), subjectText, "Girilen konu metni eşleşmiyor.");
-////            System.out.println("Subject alanı başarıyla bulundu ve test edildi.");
-//
-//        } catch (NoSuchElementException e) {
-//            // Element bulunamazsa bu bloğa girer
-//            System.err.println("Hata: Subject alanı bulunamadı! Test adımı atlandı.");
-//            e.printStackTrace(); // Hata detaylarını konsola yazdır (isteğe bağlı)
-//            // Assert.fail("Subject alanı bulunamadı!"); // Testi doğrudan başarısız etmek isterseniz
-//        } catch (Exception e) {
-//            // Diğer olası hataları yakalar
-//            System.err.println("Beklenmeyen bir hata oluştu: " + e.getMessage());
-//            e.printStackTrace();
-//        } finally {
-//            // Bu blok, try veya catch bloklarından sonra her zaman çalışır (isteğe bağlı)
-//            System.out.println("Subject alanı testi tamamlandı.");
-//            // Gerekirse temizleme işlemleri burada yapılabilir.
-//        }
-//
-//        practiceFormPage.getHobiesSports().click();Thread.sleep(2000);
-//        practiceFormPage.getHobiesReading().click();Thread.sleep(2000);
-//        practiceFormPage.getHobiesMusic().click();Thread.sleep(2000);
-//
-//        practiceFormPage.getSelectPicture().click();
-//        String uploadPath = "C:/Users/Msi/Downloads/images.jpeg";
-//        String expectedFileName = "images.jpeg";
-//
-//        practiceFormPage.getSelectPicture().sendKeys(uploadPath);//Choosefile butonuyla upload testi yapıyor.
-//        Thread.sleep(3000);
-//        org.testng.Assert.assertTrue(practiceFormPage.getSelectPicture().isDisplayed());
-//        try {
-//            if (practiceFormPage.getSelectPicture().isDisplayed()) {
-//                System.out.println("Dosya başarıyla yüklendi!");
-//            } else {
-//                System.out.println("Yükleme başarısız!");
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-////    @Test(priority = 3)
-////    public void firstName() throws InterruptedException {
-////        practiceFormPage.getFirstName().click();Thread.sleep(3000);
-////        practiceFormPage.getFirstName().sendKeys("Hakan");
-////        practiceFormPage.getFirstName().click();
-//////        jsx.executeScript("window.scrollBy(0,550)");
-//////        practiceFormPage.getSubmitButton().click();Thread.sleep(3000);
-//////        System.out.println(practiceFormPage.getFirstName().getText());
-//////        Assert.assertTrue(practiceFormPage.getFirstName().getText()
-//////                .equalsIgnoreCase("Hakan"));
-////
-////        practiceFormPage.getLastName().click();//Thread.sleep(3000);
-////        practiceFormPage.getLastName().sendKeys("Basar");
-////        System.out.println(practiceFormPage.getLastName().getText());
-////        jsx.executeScript("window.scrollBy(0,550)");
-////        practiceFormPage.getSubmitButton().click();Thread.sleep(3000);
-////        System.out.println(practiceFormPage.getLastName().getText());
-////
-//////        practiceFormPage.getFirstNameId().click();
-//////        practiceFormPage.getFirstNameId().sendKeys("Hakan");
-//////        practiceFormPage.getFirstNameId().click();
-//////        jsx.executeScript("window.scrollBy(0,550)");
-//////        practiceFormPage.getSubmitButton().click();
-//////        System.out.println(practiceFormPage.getFirstNameId().getText());
-//////        Assert.assertTrue(practiceFormPage.getFirstNameId().getText()
-//////                .equals("Hakan"));
-////    }
 @Test (priority = 2)
 public void practiceFormBeginningFulling2() throws InterruptedException, AWTException {
     practiceFormPage.getFirstName().click();//Thread.sleep(3000);
@@ -204,7 +98,29 @@ public void practiceFormBeginningFulling2() throws InterruptedException, AWTExce
     practiceFormPage.getSeptember().click();//Thread.sleep(3000);
     practiceFormPage.getYirmiUcuncuGun().click();//Thread.sleep(3000);
 
-    jsx.executeScript("window.scrollBy(0,400)");
+//    actions.sendKeys(Keys.ENTER);Thread.sleep(3000);
+    jsx.executeScript("window.scrollBy(0,200)");Thread.sleep(3000);
+    actions.sendKeys(Keys.TAB).perform();Thread.sleep(3000);
+    practiceFormPage.getSubjects().isDisplayed();
+    System.out.println(practiceFormPage.getSubjects().isDisplayed());
+    practiceFormPage.getSubjects().isEnabled();
+    System.out.println(practiceFormPage.getSubjects().isEnabled());
+    practiceFormPage.getSubjects().click();
+//    practiceFormPage.getSubjects().sendKeys("mat");//sendKeys("") ile olmayacak sanırım, baska bisey istiyor buyuk ıhtımal.
+//    actions.sendKeys(Keys.ENTER);Thread.sleep(3000);//Math secimi
+//    actions.sendKeys(Keys.ARROW_DOWN).build().perform();
+//    actions.sendKeys(Keys.ARROW_UP).build().perform();
+//    actions.sendKeys(Keys.ENTER).build().perform();Thread.sleep(3000);//asagıdaki ve yukarıdaki secimleri yoruma aldık.
+
+    JavascriptExecutor js = (JavascriptExecutor)driver;
+    //js.executeScript("arguments[0].value='mat';", practiceFormPage.getSubjects());
+
+    actions.moveToElement(practiceFormPage.getSubjects()).click().sendKeys("mat").perform();
+    actions.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
+
+    Thread.sleep(3000);
+
+    jsx.executeScript("window.scrollBy(0,200)");
 
     practiceFormPage.getHobiesSports().click();//Thread.sleep(2000);
     practiceFormPage.getHobiesReading().click();//Thread.sleep(2000);
